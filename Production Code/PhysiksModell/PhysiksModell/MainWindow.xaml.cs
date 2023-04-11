3b1467f90ae960e094c21476c787e2a57cbdb57f
Jordan <JordanGratn16@hotmail.com>
2023-04-11 16:25:42 +0200

    Added Gui Elements & set up Timer for animation

---
+++ Production Code/PhysiksModell/PhysiksModell/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace PhysiksModell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class View : Window
+    {
+        private readonly DispatcherTimer animationInterval = new DispatcherTimer();
+        bool placementMode;
+        public View()
+        {
+            //Default Global Variable Settings//-----------------------------
+            placementMode = false;
+            //GUI Initialization//-------------------------------------------
+            InitializeComponent();
+            //Timer setup for animation Frames//-----------------------------
+            animationInterval.Interval = TimeSpan.FromMilliseconds(100);
+            animationInterval.Tick += AnimationUpdate;
+        }
+        private void AnimationUpdate(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void buttonPlaceBall_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
