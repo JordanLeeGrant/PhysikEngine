d7e9236ee384e52fad97404fba24d201162d2dea
Jordan <JordanGratn16@hotmail.com>
2023-04-11 18:11:58 +0200

    Added updated Coordinate Tracking when Clicking "buttonPlaceBall"

--- Production Code/PhysiksModell/PhysiksModell/MainWindow.xaml.cs
+++ Production Code/PhysiksModell/PhysiksModell/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace PhysiksModell
             placementMode = false;
             //GUI Initialization//-------------------------------------------
             InitializeComponent();
-            //Grid Settup//----------------------------------------------
+            //Grid Settup//--------------------------------------------------
             
             //Timer setup for animation Frames//-----------------------------
             animationInterval.Interval = TimeSpan.FromMilliseconds(100);
@@ -36,15 +36,33 @@ namespace PhysiksModell
         }
         private void AnimationUpdate(object sender, EventArgs e)
         {
-
+            if (!placementMode){
+                animationInterval.Stop();
+                return;
+            }
+            int[] pos = CurrentMousePosition();
+            //Labels--------------------------------------------------------- 
+            //Check if on Canvas for X
+            if (pos[0] < 0) { labelXCoord.Content = "|X| : Invalid"; }
+            else if (pos[0] > canvasSimulationSpace.ActualWidth) { labelXCoord.Content = "|X| : Invalid"; }
+            else { labelXCoord.Content = "|X| :" + pos[0]; }
+            //Check if on Canvas for Y
+            if (pos[1] < 0) { labelYCoord.Content = "|Y| : Invalid"; }
+            else if(pos[1] > canvasSimulationSpace.ActualHeight) { labelYCoord.Content = "|Y| : Invalid"; }
+            else { labelYCoord.Content = "|Y| :" + pos[1]; }
 
         }
 
         private void buttonPlaceBall_Click(object sender, RoutedEventArgs e)
         {
-            if (placementMode != true) { return; }
+            placementMode = true;
+            animationInterval.Start();
+        }
+        private int[] CurrentMousePosition()
+        {
+            int[] mPosition = new int[] { (int)Mouse.GetPosition(canvasSimulationSpace).X, (int)Mouse.GetPosition(canvasSimulationSpace).Y };
 
-
+            return mPosition;
         }
         private void CreateBox()
         {
